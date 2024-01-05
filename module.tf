5fbb1e7960c0d6cca15ce56075069140f73dc372
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2024-01-05 08:41:57 -0500

    Updates to try

--- module.tf
+++ module.tf
@@ -16,20 +16,20 @@ resource "azurerm_key_vault" "akv" {
   tags                = local.tags
   sku_name            = var.akv_config.sku_name
 
-  enabled_for_disk_encryption     = lookup(var.akv_config.akv_features, "enabled_for_disk_encryption", null)
-  enabled_for_deployment          = lookup(var.akv_config.akv_features, "enabled_for_deployment", null)
-  enabled_for_template_deployment = lookup(var.akv_config.akv_features, "enabled_for_template_deployment", null)
-  purge_protection_enabled        = lookup(var.akv_config.akv_features, "purge_protection_enabled", null)
-  public_network_access_enabled   = lookup(var.akv_config.akv_features, "public_network_access_enabled", false)
+  enabled_for_disk_encryption     = try(var.akv_config.akv_features.enabled_for_disk_encryption, null)
+  enabled_for_deployment          = try(var.akv_config.akv_features.enabled_for_deployment, null)
+  enabled_for_template_deployment = try(var.akv_config.akv_features.enabled_for_template_deployment, null)
+  purge_protection_enabled        = try(var.akv_config.akv_features.purge_protection_enabled, null)
+  public_network_access_enabled   = try(var.akv_config.akv_features.public_network_access_enabled, false)
 
   dynamic "network_acls" {
-    for_each = lookup(var.akv_config, "network_acls", {}) != {} ? [1] : []
+    for_each = try(var.akv_config.network_acls, {}) != {} ? [1] : []
 
     content {
-      default_action             = lookup(var.akv_config.network_acls, "default_action", null)
-      bypass                     = lookup(var.akv_config.network_acls, "bypass", null)
-      ip_rules                   = lookup(var.akv_config.network_acls, "ip_rules", null)
-      virtual_network_subnet_ids = lookup(var.akv_config.network_acls, "virtual_network_subnet_ids", null)
+      default_action             = try(var.akv_config.network_aclsdefault_action, null)
+      bypass                     = try(var.akv_config.network_aclsbypass, null)
+      ip_rules                   = try(var.akv_config.network_aclsip_rules, null)
+      virtual_network_subnet_ids = try(var.akv_config.network_aclsvirtual_network_subnet_ids, null)
     }
   }
 }
